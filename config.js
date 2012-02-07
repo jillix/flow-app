3d8fbdcf32563e6b3ecd267979dfeaeb6532b2e0
Adrian <adi@ottiker.com>
2012-02-07 16:40:02 +0100

    remove tabs

--- config.js
+++ config.js
@@ -5,20 +5,20 @@ this.devPort = 8008;
 this.root = __dirname;
 
 this.orientDB = {
-	
-	//create:		true,
-	host:		"localhost",
-	port:		2480,
-	name:		"mono",
-	username:	"admin",
-	password:	"admin"
+    
+    //create: true,
+    host:     "localhost",
+    port:     2480,
+    name:     "mono",
+    username: "admin",
+    password: "admin"
 };
 
 this.mongoDB = {
-	
-	name: "mono",
-	host: "localhost",
-	port: 27017
+    
+    name: "mono",
+    host: "localhost",
+    port: 27017
 };
 
 this.public_user = "1";
@@ -26,9 +26,9 @@ this.public_user = "1";
 this.default_operation = "4";
 
 this.forever = {
-	
-	maxAttempts:	23,
-	minUptime:		2000,
-	spinSleepTime:	1000,
-	mail:			"adrian@ottiker.com"
+    
+    maxAttempts:   23,
+    minUptime:     2000,
+    spinSleepTime: 1000,
+    mail:          "adrian@ottiker.com"
 };
