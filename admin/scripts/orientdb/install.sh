004839497584299e88ce419c32d148242958dcaa
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-02-13 19:35:13 +0100

    added the orientdb import script

---
+++ admin/scripts/orientdb/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT=`which $0`
+SCRIPT_DIR=`dirname $SCRIPT`
+
+
+ORIENTDB_ROOT=/Users/gabriel/Work/orientdb/
+ORIENTDB_ROOT_USER=root
+ORIENTDB_ROOT_PASSWORD=`grep "name=\"$ORIENTDB_ROOT_USER\"" $ORIENTDB_ROOT/config/orientdb-server-config.xml | cut -d \" -f 4`
+
+
+echo "Installing the OrientDB database..."
+echo $ORIENTDB_ROOT_PASSWORD
+
+
+$ORIENTDB_ROOT/bin/console.sh $SCRIPT_DIR/mono.sql
+
