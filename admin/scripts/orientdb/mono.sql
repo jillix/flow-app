751e7fc04c3f9b90fefd152ec05426c86571f5a3
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-02-22 09:58:05 +0100

    Removed hardcoded paths from mono.sql OrientDB install script

--- admin/scripts/orientdb/mono.sql
+++ admin/scripts/orientdb/mono.sql
@@ -1,11 +1,11 @@
 drop database remote:localhost/mono root @ORIENTDB_ROOT_PASSWORD@;
 create database remote:localhost/mono root @ORIENTDB_ROOT_PASSWORD@ local;
 
-import database /Users/gabriel/Work/jillix/admin/scripts/orientdb/schema.json;
+import database admin/scripts/orientdb/schema.json;
 
 create index moduleNames on VModule (name) unique;
 
-import database /Users/gabriel/Work/jillix/admin/scripts/orientdb/records.json;
+import database admin/scripts/orientdb/records.json;
 
 info;
 
