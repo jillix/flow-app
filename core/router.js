df9826f816ecd18d8661cac242de78636dcd3330
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-03-12 17:26:41 +0100

    mono start with or without forever (depending if this is available in the configuratin file); added also orientsb as dependency only in package.json

--- core/router.js
+++ core/router.js
@@ -1,9 +1,9 @@
 // extend Object prototype with clone function
 Object.defineProperty(Object.prototype, "clone", {
         
-    //writeable: false, (default value)
-    //enumerable: false, (default value)
-    //configurable: false, (default value)
+    // writeable: false, (default value)
+    // enumerable: false, (default value)
+    // configurable: false, (default value)
     value: function(){
         
         function O(){}
@@ -39,61 +39,64 @@ var table = {
     'roles': 71
 };
 
-// start http server
-http.createServer(function(req, res) {
+
+var Router = exports.Router = function() {
+};
+
+
+Router.prototype.start = function() {
+
+    // start http server
+    http.createServer(function(req, res) {
     
-    var url = parse(req.url, true),
-        link = {
-            
-            req:        req,
-            res:        res,
-            query:      url.query || {},
-            pathname:   url.pathname,
-            path:       url.pathname.replace(/\/$|^\//g, "").split("/", 42),
-            host:       req.headers.host.split(":")[0].split(".").reverse()
-        };
+        var url = parse(req.url, true),
+            link = {
+                req:        req,
+                res:        res,
+                query:      url.query || {},
+                pathname:   url.pathname,
+                path:       url.pathname.replace(/\/$|^\//g, "").split("/", 42),
+                host:       req.headers.host.split(":")[0].split(".").reverse()
+            };
     
-    link.res.headers = {};
+        link.res.headers = {};
     
-    if (link.path[0] == CONFIG.operationKey) {
-        
-        operation(link);
-    }
-    else {
-        
-        var compID = traverse(url.pathname != delimiter ? url.pathname.replace(/\/$/, "") : url.pathname, table, "");
-    
-        if (typeof compID == "number") {
-        
-            //set headers
-            res.headers['content-style-type'] = "text/css";
-            res.headers['content-type']       = "text/html; charset=utf-8";
-            
-            send.ok(
-                
-                res,
-                "<!DOCTYPE html><html><head>"+
-                "<script type='text/javascript'>"+
-                "var require={"+
-                    "baseUrl:'/"+ CONFIG.operationKey +"/0',"+
-                    "deps:['comp/"+ Nscript +"']"+
-                "};"+
-                "window.onload=function(){"+
-                    "N.ok='/"+ CONFIG.operationKey +"';"+
-                    "N.comp('body','"+ compID +"')"+
-                "}"+
-                "</script>"+
-                "<script src='/"+ CONFIG.operationKey +"/0/comp/require.js'></script>"+
-                "</head><body></body></html>"
-            );
+        if (link.path[0] == CONFIG.operationKey) {
+            operation(link);
         }
         else {
+
+            var compID = traverse(url.pathname != delimiter ? url.pathname.replace(/\/$/, "") : url.pathname, table, "");
+
+            if (typeof compID == "number") {
             
-            send.notfound(res);
+                // set headers
+                res.headers['content-style-type'] = "text/css";
+                res.headers['content-type']       = "text/html; charset=utf-8";
+                
+                send.ok(res,
+                    "<!DOCTYPE html><html><head>"+
+                    "<script type='text/javascript'>"+
+                    "var require={"+
+                        "baseUrl:'/"+ CONFIG.operationKey +"/0',"+
+                        "deps:['comp/"+ Nscript +"']"+
+                    "};"+
+                    "window.onload=function(){"+
+                        "N.ok='/"+ CONFIG.operationKey +"';"+
+                        "N.comp('body','"+ compID +"')"+
+                    "}"+
+                    "</script>"+
+                    "<script src='/"+ CONFIG.operationKey +"/0/comp/require.js'></script>"+
+                    "</head><body></body></html>"
+                );
+            }
+            else {
+                send.notfound(res);
+            }
         }
-    }
-    
-}).listen(CONFIG.dev ? CONFIG.devPort : CONFIG.port);
+    }).listen(CONFIG.dev ? CONFIG.devPort : CONFIG.port);
+};
+
 
 /*
     borrowed logic from https://github.com/flatiron/director/blob/master/lib/director/router.js
--- core/server.js
+++ core/server.js
@@ -4,6 +4,7 @@ var nodemailer = require("nodemailer"),
 var params    = [process.argv[2] || __dirname + "/config.js"],
     lastError = "";
 
+
 var Server = exports.Server = function (config) {
     var self = this;
 
@@ -13,13 +14,13 @@ var Server = exports.Server = function (config) {
     var monoServerPath = config.root + "/" + config.monoServer;
 
     if (config.forever) {
-        var forever = require('forever');
+        var forever = require("forever");
         self.server = new forever.Monitor(monoServerPath, config.forever);
         addHandlers(self.server, self.config);
     }
     else {
-        var router = require(monoServerPath);
-        self.server = new router();
+        var Router = require(monoServerPath).Router;
+        self.server = new Router();
     }
 }
 
@@ -31,46 +32,45 @@ Server.prototype.start = function() {
 
 function addHandlers(server, config) {
 
-    //file load error
+    // file load error
     server.on("error", function() {
-    
         util.log("Error: Loading HTTP Server [Parameter: " + params.join(", ") + "] as daemon failed!");
     });
-    
-    //process error
+
+    // process error
     server.on("stderr", function(err) {
-        
+
         console.log("stderr");
-    
+
         lastError = err.toString("utf8");
     });
-    
+
     var transport = nodemailer.createTransport("Sendmail");
-    
-    //restart event
+
+    // restart event
     server.on("restart", function() {
     
-        util.log("Info: Restarted HTTP Server [Parameter: " + params.join( ", " ) + "]!");
-        util.log(lastError);
+        var title = "Info: Restarted HTTP Server";
+        var error = lastError || "unknown error";
+
+        util.log(title);
+        util.log(error);
 
         if (config.adminEmail) {
-    
-            nodemailer.send_mail(
-                {
-                    transport: transport,
-                    from:    "admin@jillix.com",
-                    to:      config.adminEmail,
-                    subject: "Notification: HTTP Server [ Parameter: " + params.join( ", " ) + " ] crashed and restarted!",
-                    body:    "Error Log: \n" + lastError
-                },
-                function(err, success) {
-                
-                    if (!success) {
-                        
-                        util.log("Error: Notification sending failed!");
-                    }
+   
+            var mail = {
+                transport: transport,
+                from:    "admin@jillix.com",
+                to:      config.adminEmail,
+                subject: title,
+                body:    "Error Log: \n" + error
+            };
+
+            nodemailer.send_mail(mail, function(err, success) {
+                if (!success) {
+                    util.log("Error: Notification sending failed!");
                 }
-            );
+            });
         }
     });
 }
