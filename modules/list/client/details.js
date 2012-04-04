59aa2a14efa91281141ae6dda3612e33751ce1c7
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-04-04 16:17:14 +0200

    loading users from the DB in the demo list happy bonus app

--- modules/list/client/details.js
+++ modules/list/client/details.js
@@ -2,168 +2,127 @@ define(["./history"], function(history) {
 
 var details = {
 
-	users: {
-	
-		'55132': {
-			
-			cnr: "55132",
-			customer: "Will Smith",
-			hp: 500,
-			history: []
-		},
-		
-		'13523': {
-		
-			cnr: "13523",
-			customer: "Tupac Shakur",
-			hp: 500,
-			history: []
-		},
-		
-		'24536': {
-		
-			cnr: "24536",
-			customer: "Jennifer Lopez",
-			hp: 500,
-			history: []
-		},
-		
-		'43534': {
-		
-			cnr: "43534",
-			customer: "Michael Schuhmacher",
-			hp: 500,
-			history: []
-		},
-		
-		'24522': {
-		
-			cnr: "24522",
-			customer: "Jenna Haze",
-			hp: 500,
-			history: []
-		},
-		
-		'45223': {
-		
-			cnr: "45223",
-			customer: "Roger Federer",
-			hp: 500,
-			history: []
-		}
-	},
+    users: {
 
-	/**
-		inits detail view
-		@author: faeb187
-	*/
-	init: function(){
-	
-		var self = this;
-		
-		//add hp button
-		$( '#addHP' ).click( function(){
-			
-			$( '#inpAddHP' ).show();
-		});
-	
-		//make hp editable
-		$( '#hp' ).live( "click", function(){
-		
-			var lab = this,
-				$inp = $( '<input/>',{
-			
-					id: "inpSetHP",
-					'class': "right",
-					type: "number",
-					value: lab.innerHTML
-				
-				}).css( "width", ( parseInt( $( lab ).css( "width" ) ) + 30 ) + "px" );
-		
-			$( lab ).replaceWith( $inp );
-			
-			$( '#addHP' ).hide();
-			
-			$inp.focus().select();
-		});
-		
-		//set/add hp
-		$( '#inpAddHP, #inpSetHP' ).live( "keypress", function( e ){
-		
-			if( e.which != 13 ) return;
-			
-			var inp = this,
-				add = inp.id == "inpAddHP" ? 1 : 0,
-				usr = self.users[ $( '.selected' ).text() ],
-				val = parseInt( inp.value ),
-				addedVal = usr.hp += val,
-				hpNew = add ? addedVal : val;
-			
-			if( add ){
-			
-				$( '#hp' ).text( hpNew );
-				$( inp ).hide();
-			}
-			
-			else {
-			
-				var $lab = $( '<label/>',{
-			
-					id: "hp",
-					'class': inp.className + " editable"
-			
-				}).text( val );
-				
-				$( inp ).replaceWith( $lab );
-			}
-			
-			$( '#addHP' ).show();
-			
-			//add entry to history
-			history.update({
-				
-				usr: usr,
-				mode: add ? "add" : "set",
-				hp: val,
-				hpNew: hpNew,
-				undoVal: usr.hp
-			});
-			
-			//REQUEST
-			//save hp
-			usr.hp = hpNew;
-		});
-	},
-	
-	/**
-		updates detail view
-		@author: faeb187
-	*/
-	update: function( cnr ){
-	
-		//REQUEST
-		var usr = this.users[ cnr ];
-				
-		$( '.cnr' ).text( usr.cnr );
-		$( '.customer' ).text( usr.customer );
-		$( '#hp' ).text( usr.hp );
-		
-		$( '.jxdetail > .detail' ).show();
-		
-		history.update({
-		
-			usr: usr
-		});
-	},
-	
-	/**
-		resets detail view
-		@author: faeb187
-	*/
-	reset: function(){
-	
-		$( '.jxdetail > .detail' ).hide();
-	}
+        '12268': {
+
+            cnr: "12268",
+            customer: "Will Smith",
+            hp: 500,
+            history: []
+        }
+    },
+
+    /**
+        inits detail view
+        @author: faeb187
+    */
+    init: function(){
+
+        var self = this;
+
+        //add hp button
+        $( '#addHP' ).click( function(){
+
+            $( '#inpAddHP' ).show();
+        });
+
+        //make hp editable
+        $( '#hp' ).live( "click", function(){
+
+            var lab = this,
+                $inp = $( '<input/>',{
+
+                    id: "inpSetHP",
+                    'class': "right",
+                    type: "number",
+                    value: lab.innerHTML
+
+                }).css( "width", ( parseInt( $( lab ).css( "width" ) ) + 30 ) + "px" );
+
+            $( lab ).replaceWith( $inp );
+
+            $( '#addHP' ).hide();
+
+            $inp.focus().select();
+        });
+
+        //set/add hp
+        $( '#inpAddHP, #inpSetHP' ).live( "keypress", function( e ){
+
+            if( e.which != 13 ) return;
+
+            var inp = this,
+                add = inp.id == "inpAddHP" ? 1 : 0,
+                usr = self.users[ $( '.selected' ).text() ],
+                val = parseInt( inp.value ),
+                addedVal = usr.hp += val,
+                hpNew = add ? addedVal : val;
+
+            if( add ){
+
+                $( '#hp' ).text( hpNew );
+                $( inp ).hide();
+            }
+
+            else {
+
+                var $lab = $( '<label/>',{
+
+                    id: "hp",
+                    'class': inp.className + " editable"
+
+                }).text( val );
+
+                $( inp ).replaceWith( $lab );
+            }
+
+            $( '#addHP' ).show();
+
+            //add entry to history
+            history.update({
+
+                usr: usr,
+                mode: add ? "add" : "set",
+                hp: val,
+                hpNew: hpNew,
+                undoVal: usr.hp
+            });
+
+            //REQUEST
+            //save hp
+            usr.hp = hpNew;
+        });
+    },
+
+    /**
+        updates detail view
+        @author: faeb187
+    */
+    update: function( cnr ){
+
+        // get the user details
+        N.link({ url: "/3/" + cnr }, function(err, usr) {
+
+            $( '.cnr' ).text( usr.system.cnr );
+            $( '.customer' ).text( usr.adress.work.company || usr.adress.work.addition );
+            $( '#hp' ).text( usr.happybonus ? usr.happybonus.points : "No HB!" );
+            $( '.jxdetail > .detail' ).show();
+
+            // now show the user happy bonus history
+            history.update({ usr: usr });
+        });
+
+    },
+
+    /**
+        resets detail view
+        @author: faeb187
+    */
+    reset: function(){
+
+        $( '.jxdetail > .detail' ).hide();
+    }
 };
 
 return details;
--- modules/list/ops.js
+++ modules/list/ops.js
@@ -1,11 +1,53 @@
 var model = require(CONFIG.root + "/core/model/orient"),
-    send = require(CONFIG.root + "/core/send.js").send;
+    send  = require(CONFIG.root + "/core/send.js").send,
+    mongo = require(CONFIG.root + "/db/mongo").db;
 
 exports.getData = function(link) {
 
-    model.getComponents(function(err, results) {
+    mongo("sag-shops", "users_happy", function(err, db) {
 
-        send.ok(link.res, results);
+        if (err) {
+            send.internalservererror(link, err);
+            return;
+        }
+
+        // all users
+        if (link.path.length == 2) {
+
+            db.find({}, { "system.cnr": 1, "_id": 0 }, { limit: 100 }).toArray(function(err, docs) {
+
+                if (err) {
+                    send.internalservererror(link, err);
+                    return;
+                }
+
+                send.ok(link.res, docs);
+            });
+        }
+        // one user
+        else if (link.path.length == 3) {
+
+            var fields = {
+               "_id": 0,
+               "system.cnr": 1,
+               "adress.work": 1,
+               "happybonus": 1
+            };
+
+            db.findOne({ "system.cnr": link.path[2] }, fields, function(err, doc) {
+
+                if (err) {
+                    send.internalservererror(link, err);
+                    return;
+                }
+
+                send.ok(link.res, doc);
+            });
+        }
+        // invalid
+        else {
+            send.notfound(link);
+        }
     });
 };
 
