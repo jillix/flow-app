895ec096d4148b9397b2325694c420dfcb335742
Adrian <adi@ottiker.com>
2012-05-04 17:51:23 +0200

    working on admin module

---
+++ modules/jillix/admin/client/main.js
@@ -0,0 +1,45 @@
+define(["faeb187/stdl.list"]function(list) {
+    
+    var Vertex = (function(V) {
+        
+        V = {
+            
+            
+        };
+        
+        return function(config) {
+            
+            var vertex = N.clone(V);
+            
+            // TODO configure vertex
+            
+            return vertex;
+        };
+        
+    })();
+    
+    var Edge = (function(E){
+        
+        E = {
+            
+            
+        };
+        
+        return function(config){
+            
+            var edge = N.clone(E);
+            
+            // TODO configure edge
+            
+            return edge;
+        };
+    })();
+    
+    return {
+        
+        init: function(config) {
+            
+            
+        }
+    }
+});
