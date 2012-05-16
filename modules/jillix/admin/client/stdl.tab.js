6f9aed5a9b59493caec3891ce87684eab8002d14
Adrian <adi@ottiker.com>
2012-05-16 17:38:11 +0200

    updated adioo/bind

--- modules/jillix/admin/client/stdl.tab.js
+++ modules/jillix/admin/client/stdl.tab.js
@@ -1,19 +1,35 @@
 define(["adioo/bind/main"], function(Bind){
     
-    var test = {
+    var Tab = {
         
-        name: "ruedi",
-        hoi: function() {
+        show: function() {
             
-            console.log(arguments[0] + this.name);
         }
     };
     
-    N.obs("myObs").l("eventName", function() {
+    return function(config) {
         
-        console.log(arguments[0]);
-    });
-    
+        config = {
+            
+            item:       "li",
+            itemHTML:   "<span class='content1'></span>",
+            paneHTML:   "<div></div>",
+            content: [
+                {
+                    
+                }
+            ]
+        };
+        
+        // 1. get dom refs or set mode to create
+        
+        var tab = N.clone(Tab);
+        
+        return tab;
+    }; 
+});
+
+/*
     //set default values
     var bind = Bind({
         
@@ -76,31 +92,4 @@ define(["adioo/bind/main"], function(Bind){
             }
         }
     });
-    
-    var Tab = {
-        
-        show: function() {
-            
-        }
-    };
-    
-    return function(config) {
-        
-        config = {
-            
-            item:       "li",
-            itemHTML:   "<span class='content1'></span>",
-            content: [
-                {
-                    
-                }
-            ]
-        };
-        
-        // 1. get dom refs or set mode to create
-        
-        var tab = N.clone(Tab);
-        
-        return tab;
-    }; 
-});
+*/
