29ccd0e09a577692edd5c4be2fd4c428ced6eb7e
Adrian <adi@ottiker.com>
2012-07-17 17:20:15 +0200

    created get ip adress utility tool

--- core/util.js
+++ core/util.js
@@ -1,4 +1,5 @@
-var fs = require( "fs" );
+var fs = require("fs");
+var os = require("os");
 
 function _require(file) {
     
@@ -157,4 +158,31 @@ this.read = function(file, encoding, callback) {
             }
         });
     }
+};
+
+//get ip adress
+this.ip = function(version, internal) {
+    
+    var netIf = os.networkInterfaces();
+    
+    version = version || 4;
+    internal = internal ? true : false;
+    
+    for (var netIfName in netIf) {
+    
+        for (var i = 0, l = netIf[netIfName].length; i < l; ++i) {
+            
+            if (
+                netIf[netIfName][i] &&
+                netIf[netIfName][i].internal == internal &&
+                netIf[netIfName][i].family.toLowerCase() === "ipv" + version &&
+                netIf[netIfName][i].address
+            ) {
+                
+               return netIf[netIfName][i].address;
+            }
+        };
+    }
+    
+    return null;
 };
