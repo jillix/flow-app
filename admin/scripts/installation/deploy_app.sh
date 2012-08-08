659794193091274dd6c6ce312170f5de69854499
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-08-08 17:42:07 +0200

    Added deploy application script

---
+++ admin/scripts/installation/deploy_app.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+zip test ./* > /dev/null
+
+MONO_ROOT=~/mono
+APP_FILE_ARG=$1
+
+function ce {
+    if [ $? -ne 0 ]
+    then
+        if [ "$1" != "" ]
+        then
+            echo "$1"
+        fi
+        rm -f $APP_FILE_ARG
+        exit 1
+    fi
+}
+
+if [ ! -f "$APP_FILE_ARG" ]
+then
+    echo "Missing application archive" 1>&2
+    exit 1
+fi
+
+TMP_APP_DIR=`mktemp -d $MONO_ROOT/apps/tmp/XXXXXX 2> /dev/null`
+ce "Failed to create application temporary directory"
+
+# unzip application archive in mono apps
+unzip $APP_FILE_ARG -d $TMP_APP_DIR > /dev/null
+ce "Could not unarchive the application archive."
+
+# delete archive
+rm $APP_FILE_ARG
+
+# read descriptos file
+APP_ID=`node -e "console.log(require('$TMP_APP_DIR/mono.json').appId)" 2> /dev/null`
+ce "Could not determine the application ID."
+
+# move the app to the app ID directory
+if [ -e "$MONO_ROOT/apps/$APP_ID" ]
+then
+    rm -R "$MONO_ROOT/apps/$APP_ID"
+    ce "Could not cleanup already existing application: $APP_ID"
+fi
+mv "$TMP_APP_DIR" "$MONO_ROOT/apps/$APP_ID"
+ce "Could not move application to propper location: $APP_ID"
+
+cd $MONO_ROOT
+npm install > /dev/null
+ce "Could not deploy application: $APP_ID"
+
+echo "Succesfully deployed application $APP_ID"
+
