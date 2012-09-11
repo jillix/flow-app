0ce86eaa1a36f282828d0cfab22b9ceab3474e55
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-09-11 14:13:55 +0000

    Removed bash prompt coloring because it was messing with the prompts

--- admin/scripts/keep_alive.sh
+++ admin/scripts/keep_alive.sh
@@ -17,7 +17,7 @@ DEPLOY_LOG=~/logs/deploy.log
 
 ORIENT_PID=`lsof -iTCP:2424 -sTCP:LISTEN -t`
 ORIENT_SCREEN_NAME=orient
-ORIENT_SERVER=~/mono/bin/orientdb/bin/server.sh
+ORIENT_SERVER="cd ~/mono/bin/orientdb/bin; ./server.sh"
 ORIENT_LOG=~/logs/orient.log
 
 
@@ -46,27 +46,31 @@ check_screen() {
         sleep 1 # may be necessary
         screen -S $1 -X detach
 
-        # now give it a color
-        if [ "$1" == "mono" ]
-        then
-            NEW_PS1='${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\w\$ \[\e[1;36m\]'
-        elif [ "$1" == "deploy" ]
-        then
-            NEW_PS1='${debian_chroot:+($debian_chroot)}\[\033[01;35m\]\u@\h\[\033[00m\]:\w\$ \[\e[1;35m\]'
-        else
-            NEW_PS1=$PS1
-        fi
-        screen -S $1 -X stuff "PS1='$NEW_PS1'"
-        new_line $1
+        ## now give it a color
+        #if [ "$1" == "mono" ]
+        #then
+        #    NEW_PS1='${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\w\$ \[\e[1;36m\]'
+        #elif [ "$1" == "deploy" ]
+        #then
+        #    NEW_PS1='${debian_chroot:+($debian_chroot)}\[\033[01;35m\]\u@\h\[\033[00m\]:\w\$ \[\e[1;35m\]'
+        #elif [ "$1" == "orient" ]
+        #then
+        #    NEW_PS1='\[\033[01;35m\]\u@\h\[\033[00m\]:\w$ \[\e[1;33m\]'
+        #else
+        #    NEW_PS1=$PS1
+        #fi
+
+        #screen -S $1 -X stuff "PS1='$NEW_PS1'"
+        #new_line $1
     fi
 }
 
 execute_in_screen() {
     check_screen $1
-    COMMAND=`echo -e "node $2 2>> $3 >> $3\n"`
+    COMMAND=`echo -e "node $2 &>> $3\n"`
     if [ "$1" == "$ORIENT_SCREEN_NAME" ]
     then
-        COMMAND=`echo -e "$2 2>> $3 >> $3\n"`
+        COMMAND=`echo -e "$2 &>> $3\n"`
     fi
     screen -S $1 -X stuff "$COMMAND"
     new_line $1
