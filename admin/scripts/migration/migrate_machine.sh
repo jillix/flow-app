cb30f8acfab470b4e794a548e01e3c9b4304afb7
Adrian Ottiker <adrian@ottiker.com>
2013-03-09 13:19:27 +0100

    added pgrep installation to migration script

--- admin/scripts/migration/migrate_machine.sh
+++ admin/scripts/migration/migrate_machine.sh
@@ -68,6 +68,15 @@ function install {
     fi
 }
 
+# this package installs the pgrep programm
+function install_procps {
+    # linux
+    apt-get install procps
+    
+    # mac os x
+    #sudo port install proctools
+}
+
 function install_nginx {
 
     INFO=`dpkg -s nginx 2>/dev/null`
@@ -254,9 +263,12 @@ function setup_user {
 }
 
 function install_software {
-
+    
     # needed to add the apt-add-repository command
     install python-software-properties apt-add-repository
+    
+    # install pgrep
+    install_procps
 
     # install git if not present
     install git
