d5466c0ed74b0be3a146639916bef51b39f88ea6
Adrian Ottiker <adrian@ottiker.com>
2013-03-14 19:41:08 +0100

    cleaning up api, work in progress

--- api.js
+++ api.js
@@ -1,11 +1,22 @@
+/*
+
+MONO API
+
+installation/deployment
+administration
+databases
+users
+
+*/
 M = {config: require('./lib/config')};
-M.orient = require('./lib/orient');
-M.app = require('./lib/apps');
-M.module = require('./lib/modules');
-M.repo = require('./lib/repos');
-M.dir = require('./lib/directory');
-M.user = require('./lib/users');
-M.model = require('./lib/model');
-M.installation = require('./lib/installation');
+M.orient = require('./lib/api/orient');
+M.app = require('./lib/api/apps');
+M.module = require('./lib/api/modules');
+M.repo = require('./lib/api/repos');
+M.dir = require('./lib/api/directory');
+M.user = require('./lib/api/users');
+M.model = require('./lib/api/model');
+M.session = require('./lib/api/sessions');
+M.installation = require('./lib/api/installation');
 
 module.exports = M;
