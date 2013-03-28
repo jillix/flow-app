42e590f24c3a05152e5e0f0650a606b9d4d63d1b
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-03-28 12:28:49 +0100

    Migrated the application installation to the new API structure (not yet the uninstall part)

--- admin/scripts/install.js
+++ admin/scripts/install.js
@@ -1,2 +1,6 @@
 // load mono api
-require(process.cwd() + '/api').installation.installApps();
+require(process.cwd() + '/api');
+
+M.app.install(process.cwd() + '/apps/00000000000000000000000000000002/mono.json', function() {
+    M.orient.close();
+});
