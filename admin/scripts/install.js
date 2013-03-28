455deba3c983f332a8bfb42a8a011c53f704c5ed
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-03-28 13:36:28 +0100

    Moved operation API and model into place in the new API structure

--- admin/scripts/install.js
+++ admin/scripts/install.js
@@ -2,5 +2,6 @@
 require(process.cwd() + '/api');
 
 M.app.install(process.cwd() + '/apps/00000000000000000000000000000002/mono.json', function() {
+    // the install opens an orient connection so we have to close it when done
     M.orient.close();
 });
