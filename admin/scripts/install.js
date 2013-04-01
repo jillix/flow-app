cacf833c5de3b63dfefb0f6e63c91414fa386601
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-04-01 21:44:34 +0200

    Workaround for the orient driver thrown error when close is called before a DB was opened

--- admin/scripts/install.js
+++ admin/scripts/install.js
@@ -3,11 +3,15 @@ require(process.cwd() + '/api');
 
 function exit(err) {
 
+    if (err) {
+        console.error(err);
+    }
+
     // the install opens an orient connection so we have to close it when done
+    // TODO if the DB was not opened this call will throw an error
     M.orient.close();
 
     if (err) {
-        console.error(err);
         process.exit(1);
     }
 }
