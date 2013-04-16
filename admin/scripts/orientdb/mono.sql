5b94ef8aee1e8a4abbe935bec158c27cb227f2d5
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-04-16 11:48:30 +0200

    Added remove datasources to the uninstall and added unique constraint index an app datarource based on name

--- admin/scripts/orientdb/mono.sql
+++ admin/scripts/orientdb/mono.sql
@@ -6,6 +6,7 @@ create index moduleNames on VModule (source, owner, name) unique;
 create index applicationIds on VApplication (id) unique;
 create index applicationRoles on VRole (app, name) unique;
 create index domainNames on VDomain (name) unique;
+create index datasourceNames on VDatasource (app, name) unique;
 
 import database admin/scripts/orientdb/records.json;
 
