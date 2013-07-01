0adae1775ae4c4c1c5a094d003a9b3223b78dc6b
Adrian Ottiker <adrian@ottiker.com>
2013-07-01 17:42:43 +0200

    use mongodb as core db, replaced appid with mongoid

--- api.js
+++ api.js
@@ -1,21 +1,38 @@
 
 M = { config: require('./lib/config') };
-
-M.error = require('./lib/api/error');
 M.mongo = require('./lib/api/db/mongo');
-M.util = require('./lib/api/util');
-M.fs = require('./lib/api/fs');
-M.repo = require('./lib/api/repo');
 
-M.app = require('./lib/api/app');
-M.module = require('./lib/api/module');
+module.exports = function (callback) {
+    M.mongo.connect(M.config.mongoDB.name, function (err, db) {
 
-M.operation = require('./lib/api/operation');
-M.session = require('./lib/api/session');
-M.datasource = require('./lib/api/datasource');
-M.database = require('./lib/api/database');
+        if (err) {
+            console.error("Could not connect to MongoDB");
+            console.error(err);
+            process.exit(2);
+        }
 
-M.runtime = require('./lib/api/runtime');
+        M.db = db;
 
-module.exports = M;
+        if (M.config.app) {
+            M.config.app = M.mongo.ObjectID(M.config.app);
+        }
+
+        M.error = require('./lib/api/error');
+        M.util = require('./lib/api/util');
+        M.fs = require('./lib/api/fs');
+        M.repo = require('./lib/api/repo');
+
+        M.app = require('./lib/api/app');
+        M.module = require('./lib/api/module');
+
+        M.operation = require('./lib/api/operation');
+        M.session = require('./lib/api/session');
+        M.datasource = require('./lib/api/datasource');
+        M.database = require('./lib/api/database');
+
+        M.runtime = require('./lib/api/runtime');
+
+        callback(null);
+    });
+};
 
