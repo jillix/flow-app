36598e4c6766e4c3d45cd09af667655a57ca0c52
Adrian Ottiker <adrian@ottiker.com>
2013-10-03 16:44:29 +0200

    create machine install script, which installs mongodb and nodejs

---
+++ admin/scripts/install_machine.sh
@@ -0,0 +1,14 @@
+#mongodb
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo touch /etc/apt/sources.list.d/mongodb.list
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+
+#nodejs and npm
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
