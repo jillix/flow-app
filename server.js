6e31ef6b4589715dcf5e714ad634d522ad584f8a
Adrian Ottiker <adrian@ottiker.com>
2015-01-19 17:22:59 +0100

    moved request/message handler into separate modules and included the server config

--- server.js
+++ server.js
@@ -1,193 +1,102 @@
-require('./lib/server/config');
+var path = require('path');
+var engine_root = path.normalize(__dirname + '/');
+var lib = engine_root + 'lib/';
+
+// ------------------------------------------------------------------ CLI CHECKS
+// read repo path
+var repo = process.argv[2];
+if (!repo) {
+    throw new Error('No repo path argument.');
+}
+
+// get the path to the repo
+repo = repo[0] === '/' ? repo : path.normalize(engine_root + '/../' + repo) + '/';
+
+// get port
+if (!process.argv[3] || !process.argv[3].replace(/[^0-9]/g, '')) {
+    throw new Error('Not port argument.');
+}
+process.env.Z_PORT = process.argv[3];
+
+// check production mode
+if (process.argv[4] === 'PRO') {
+    process.env.Z_PRODUCTION = 'true';
+}
+
+// --------------------------------------------------------------- ENV VARIABLES
+// access values
+process.env.Z_USER = '530639e09060f4703737e017';    // TODO get value from env
+process.env.Z_KEY = '0ULtAr8iH151p69q2XYTCht';      // TODO get value from env
+
+// system roles
+process.env.Z_ROLE_MODULE = '_module';
+
+// session values
+process.env.Z_SESSION_PUBLIC = '*';
+process.env.Z_SESSION_ROLE_KEY = 'role';
+process.env.Z_SESSION_LOCALE_KEY = 'locale';
+process.env.Z_SESSION_USER_KEY = 'user';
+
+// engine values
+process.env.Z_OP_KEY = '@';
+process.env.Z_CORE_INST = 'Z';
+process.env.Z_CLIENT_LIBS = repo + 'libs.json';
+
+// module role
+process.env.Z_ROLE_MODULE = 'module';
+
+// send events
+process.env.Z_SEND_INST_REQ = 'I>';
+process.env.Z_SEND_MODEL_REQ = 'M>';
+process.env.Z_SEND_QUERY_REQ = 'Q>';
+process.env.Z_SEND_VIEW_REQ = 'V>';
+process.env.Z_SEND_MODULE_REQ = 'M';
+process.env.Z_SEND_CLIENT_REQ = 'Z';
+
+// http caching
+process.env.Z_HTTP_CACHE_MAX_AGE = 86400;
+process.env.Z_HTTP_CACHE_MAX_AGE_FINGERPRINT = 94670000;
+
+// path values
+process.env.Z_PATH_ENGINE = engine_root;
+process.env.Z_PATH_CACHE = lib + 'cache/';
+process.env.Z_PATH_MODELS = lib + 'models/';
+process.env.Z_PATH_STORES = lib + 'stores/';
+process.env.Z_PATH_MIDDLEWARE = lib + 'middleware/';
+process.env.Z_PATH_SERVER = lib + 'server/';
+process.env.Z_PATH_CLIENT = lib + 'client/';
+process.env.Z_PATH_MODULE = lib + 'module/';
+process.env.Z_PATH_VIEWS = lib + 'views/';
+process.env.Z_PATH_UTILS = lib + 'utils/';
+
+process.env.Z_PATH_PROCESS_REPO = repo;
+process.env.Z_PATH_PROCESS_PUBLIC = repo + 'public/';
+process.env.Z_PATH_PROCESS_MARKUP = repo + 'markup/';
+process.env.Z_PATH_PROCESS_MODULES = repo + 'modules/';
+process.env.Z_PATH_PROCESS_COMPOSITION = repo + 'composition/';
+
+// TODO this should be a part of the service application
+//process.env.Z_PATH_REPOS = process.env.ENGINE_APPS || process.env.HOME + '/engine_repos/';
+
 var env = process.env;
 var http = require('http');
 var WebSocketServer = require('ws').Server;
-var parse = require('url').parse;
-var clone = require(env.Z_PATH_UTILS + 'object').clone;
-var route = require(env.Z_PATH_SERVER + 'router');
-var send = require(env.Z_PATH_SERVER + 'send');
-var middleware = require(env.Z_PATH_MIDDLEWARE + 'session');
-var cache = require(env.Z_PATH_CACHE + 'cache');
 
-// create instance cache with role check
-var instanceCache = cache.pojo('instances', true);
+var middleware = require(env.Z_PATH_MIDDLEWARE + 'session');
+var requestHandler = require(env.Z_PATH_SERVER + 'request');
+var connectionHandler = require(env.Z_PATH_SERVER + 'socket');
 
 // init and cache core module instance
-require(env.Z_PATH_MODULE + 'module');
+//require(env.Z_PATH_MODULE + 'module');
 
 // start http server
-var httpServer = http.createServer(middleware(requestHandler));
+process.httpServer = http.createServer(middleware(requestHandler));
 
 // start ws server
-var ws = new WebSocketServer({server: httpServer});
-
-// pass websocket to send module
-send.setWebsocket(ws);
+process.webSocketServer = new WebSocketServer({server: process.httpServer});
 
 // listen to ws connections
-ws.on('connection', middleware(connectionHandler));
+process.webSocketServer.on('connection', middleware(connectionHandler));
 
 // start http server
-httpServer.listen(env.Z_PORT);
-
-// check if role has access to instance and operation
-function checkOperationAccess (role, instance, event) {
-
-    // get instance with role check
-    instance = instanceCache.get(instance, role);
-
-    // check if this instance has acces to the requested operation
-    if (instance && instance._access[event]) {
-        return instance;
-    }
-
-    return;
-}
-
-// handle http request
-function requestHandler (err, session, req, res) {
-
-    if (err) {
-        return send.httpError(req, res, 500, err);
-    }
-
-    var url = parse(req.url, true);
-    var path = url.pathname.replace(/\/$|^\//g, "").split("/", 42);
-
-    if (path[0] === env.Z_OP_KEY) {
-
-        if (path.length < 3) {
-            return send.httpError(req, res, 400, new Error('Invalid operation url.'));
-        }
-
-        // if no operation was found in the request URL
-        if (!path[1] || !path[2]) {
-            return send.httpError(req, res, 400, new Error('Missing module instance ID or operation name.'));
-        }
-
-        // get cached instance and check access
-        var role = session[env.Z_SESSION_ROLE_KEY];
-        var instance = checkOperationAccess(role, path[1], path[2]);
-
-        // check if instance an operation exists
-        if (instance) {
-
-            // create event object
-            var event = {
-                _: instance,
-                req: req,
-                res: res,
-                path: path.slice(3),
-                query: url.query,
-                pathname: url.pathname,
-                name: path[2],
-                send: send.link,
-                session: session
-            };
-
-            // save session in link
-            event[env.Z_SESSION_ROLE_KEY] = role;
-            event[env.Z_SESSION_USER_KEY] = session[env.Z_SESSION_USER_KEY];
-            event[env.Z_SESSION_LOCALE_KEY] = session[env.Z_SESSION_LOCALE_KEY];
-
-            // set a empty response header object
-            event.headers = {};
-
-            // emit operation
-            instance.emit(event.name, event, req, function (code, data) {
-                event.send(code, data);
-            });
-
-            // resume request
-            req.resume();
-
-        } else {
-            return send.httpError(req, res, 404, new Error('Instance or operation not found.'));
-        }
-
-    } else {
-        route(url.pathname, req, res);
-        req.resume();
-    }
-}
-
-function connectionHandler (err, session, ws) {
-
-    if (err) {
-        return send.wsError(ws, err);
-    }
-
-    // listen to messages
-    ws.on('message', function (data) {
-
-        // parse data
-        // protocoll: ["instanceName:event:msgid","data"]
-
-        /**
-            types:
-            0: Call
-            1: Event
-            ---------------------------
-            2: End
-            3: Data
-            4: Error
-
-            protocoll:
-            0, id, instance.method
-            1, id, instance.event
-            ---------------------------
-            2, id, err, data
-            3, id, data
-        */
-
-
-        try {
-            data = JSON.parse(data.toString());
-            data[0] = data[0].split(':');
-
-            // check message integrity
-            if (!data[0][0] || !data[0][1]) {
-                throw new Error('Bad message');
-            }
-        } catch (err) {
-            return send.wsError(ws, err);
-        }
-
-        // get instance from cache and check access
-        var role = session[env.Z_SESSION_ROLE_KEY];
-        var instance = checkOperationAccess(role, data[0][0], data[0][1]);
-
-        // check if instance and event exists
-        if (instance) {
-
-            // create event object
-            var event = {
-                _: instance,
-                ws: ws,
-                name: data[0][1],
-                send: send.message,
-                id: data[0][2],
-                session: session
-            };
-
-            // save session in event
-            event[env.Z_SESSION_ROLE_KEY] = role;
-            event[env.Z_SESSION_USER_KEY] = session[env.Z_SESSION_USER_KEY];
-            event[env.Z_SESSION_LOCALE_KEY] = session[env.Z_SESSION_LOCALE_KEY];
-
-            // emit event
-            instance.emit(event.name, event, data[1], function (err, data) {
-
-                // TODO implement loging
-                if (err) {
-                    console.log(instance._name, 'event:', event.name, 'error:', err);
-                }
-
-                event.send(err, data);
-            });
-
-        } else {
-            send.wsError(ws, new Error('Instance or event not found'), data[0][2]);
-        }
-    });
-}
+process.httpServer.listen(env.Z_PORT);
