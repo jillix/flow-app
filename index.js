ef9c1da5f677b211e9c18544098afe2dbe41c552
Adrian Ottiker <adrian@ottiker.com>
2015-01-21 16:24:25 +0100

    moved global static values to the engine module

--- index.js
+++ index.js
@@ -12,29 +12,5 @@ if (!repo || !port || !user || !key) {
     throw new Error('Invalid CLI arguments Example: ./index.js /absolute/path/to/repo 8000 USERID APIKEY [PRO]');
 }
 
-// system roles
-process.env.Z_ROLE_MODULE = '_module';
-
-// session values
-process.env.Z_SESSION_PUBLIC = '*';
-process.env.Z_SESSION_ROLE_KEY = 'role';
-process.env.Z_SESSION_LOCALE_KEY = 'locale';
-process.env.Z_SESSION_USER_KEY = 'user';
-
-// engine values
-process.env.Z_OP_KEY = '@';
-
-// http caching
-process.env.Z_HTTP_CACHE_MAX_AGE = 86400;
-process.env.Z_HTTP_CACHE_MAX_AGE_FINGERPRINT = 94670000;
-
-// path values
-process.env.Z_PATH_ENGINE = require('path').normalize(__dirname + '/');
-process.env.Z_PATH_PROCESS_REPO = repo;
-process.env.Z_PATH_PROCESS_PUBLIC = repo + 'public/';
-process.env.Z_PATH_PROCESS_MARKUP = repo + 'markup/';
-process.env.Z_PATH_PROCESS_MODULES = repo + 'modules/';
-process.env.Z_PATH_PROCESS_COMPOSITION = repo + 'composition/';
-
 // start engine
 require('./lib/engine')(repo, port, user, key, production);
