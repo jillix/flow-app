b7eac76dcb4ad223501681d226d6784c8a72c1a8
Adrian Ottiker <adrian@ottiker.com>
2015-10-23 15:23:09 +0000

    module installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# go through all compositions and collect module names
+# create bundles with node_modules/module/client_bundle.js
+
+# use source maps for debugging ?
+
+# minify js in production mode
+# minify css
+# minify html
+# compress files
