5786d67afbb91640bb1f07d4217cbabca2490f38
Adrian Ottiker <adrian@ottiker.com>
2015-10-30 13:14:32 +0000

    collect bund cli command examples

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@
 # important create external bundles of the top level modules!!
 # browserify -r view -o node_modules/view/M.js
 # browserify --dg false (disable detecting of nodes global (process, ect.)) also for modules??
+# compression example
+# browserify lib/flow.client.js > M.js; ccjs M.js > _tmp.js; gzip -9c _tmp.js > M.js; rm _tmp.js
+# watchify example with compression
+# watchify lib/flow.client.js -vd -o 'gzip -9 > M.js'
 # use source maps for debugging ?
 
 # minify js in production mode
