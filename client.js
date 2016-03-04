4bd7ea65fdb2033d9542ec44d4bc6f0b1f9e4912
Adrian Ottiker <adrian@ottiker.com>
2016-03-04 12:04:51 +0000

    move browser client to this repo

---
+++ client.js
@@ -0,0 +1,41 @@
+var Flow = require('flow');
+
+// init flow with core module
+module.exports = Flow({
+
+    // load module bundles and return the entry-point exports
+    mod: function (name, callback) {
+
+        var node = document.createElement('script');
+        var path = name + '.js';
+        node.onload = function () {
+            node.remove();
+            name = name[0] === '/' ? path : name;
+            callback(null, require(name));
+        };
+
+        // set url and append dom script elm to the document head
+        node.src = (name[0] === '/' ? '/_c' : '/_m/') + path;
+        document.head.appendChild(node);
+    },
+
+    // load module instance composition (MIC)
+    mic: function (name, callback) {
+
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                var composition = httpRequest.responseText;
+                if (httpRequest.status === 200) {
+                    composition = JSON.parse(composition);
+                    composition._roles = {'*': true};
+                    callback(null, composition);
+                } else {
+                    callback(new Error(httpRequest.responseText));
+                }
+            }
+        };
+        httpRequest.open('GET', '/_i/' + name + '.json');
+        httpRequest.send();
+    }
+});
