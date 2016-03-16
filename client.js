d21d67f1641a27d0999b891e7e3c56eb3458141d
Adrian Ottiker <adrian@ottiker.com>
2016-03-16 19:11:49 +0000

    fix error check; create methods outside of the handler

--- client.js
+++ client.js
@@ -1,47 +1,50 @@
 var Flow = require('flow');
-global.flow = function (event, options) {
+var adapterSet;
 
-    if (event !== 'string') {
-        throw Error('Flow: No init event.');
+// load module instance composition (MIC)
+function mic (name, callback) {
+
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            var composition = httpRequest.responseText;
+            if (httpRequest.status === 200) {
+                composition = JSON.parse(composition);
+                composition._roles = {'*': true};
+                callback(null, composition);
+            } else {
+                callback(new Error(httpRequest.responseText));
+            }
+        }
+    };
+    httpRequest.open('GET', '/_i/' + name + '.json');
+    httpRequest.send();
+}
+
+// load module
+function mod (name, callback) {
+
+    var node = document.createElement('script');
+    var path = name + '.js';
+    node.onload = function () {
+        node.remove();
+        name = name[0] === '/' ? path : name;
+        callback(null, require(name));
+    };
+
+    // set url and append dom script elm to the document head
+    node.src = (name[0] === '/' ? '/_c' : '/_m/') + path;
+    document.head.appendChild(node);
+};
+
+module.exports = function (event, options) {
+
+    if (!adapterSet) {
+        options = options || {};
+        options.mod = mod;
+        options.mic = mic;
+        adapterSet = true;
     }
 
-    options = options || {};
-
-    // load module instance composition (MIC)
-    options.mic = function (name, callback) {
-
-        httpRequest = new XMLHttpRequest();
-        httpRequest.onreadystatechange = function () {
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                var composition = httpRequest.responseText;
-                if (httpRequest.status === 200) {
-                    composition = JSON.parse(composition);
-                    composition._roles = {'*': true};
-                    callback(null, composition);
-                } else {
-                    callback(new Error(httpRequest.responseText));
-                }
-            }
-        };
-        httpRequest.open('GET', '/_i/' + name + '.json');
-        httpRequest.send();
-    };
-
-    // load module
-    options.mod = function (name, callback) {
-
-        var node = document.createElement('script');
-        var path = name + '.js';
-        node.onload = function () {
-            node.remove();
-            name = name[0] === '/' ? path : name;
-            callback(null, require(name));
-        };
-
-        // set url and append dom script elm to the document head
-        node.src = (name[0] === '/' ? '/_c' : '/_m/') + path;
-        document.head.appendChild(node);
-    };
-
-    Flow(event, options);
+    return Flow(event, options);
 };
