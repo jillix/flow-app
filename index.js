4e4b7162a94b8e8c03a841f63d00d6a096642ec7
Adrian Ottiker <adrian@ottiker.com>
2016-07-31 14:23:24 +0200

    add option to read entrypoint from a file

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 #!/usr/bin/env node
+'use strict'
 
+//import flow from 'flow';
 const flow = require('flow');
 
-// TODO add option to read entrypoint form file (container)
+// check if an file path is in argv,
+// thus read the entrypoint from there.
+if (process.argv[2]) {
+    //import {readFile} from 'fs';
+    //import {resolve} from 'path';
+    const readFile = require('fs').readFile;
+    const resolve= require('path').resolve;
+    readFile(resolve(process.argv[2]), (err, data) => {
+        if (err) {
+            throw err;
+        }
 
-process.on('message', (entrypoint) => {
+        try {
+            data = JSON.parse(data.toString());
+        } catch (err) {
+            throw err;
+        }
+
+        initEntrypoint(data);
+    })
+
+// expect a IPC message with the entrypoint.
+} else {
+    process.on('message', initEntrypoint);
+}
+
+// emit entrypoint event
+function initEntrypoint (entrypoint) {
 
     // append mandatory flow environment
     if (entrypoint.env) {
         process.flow_env = entrypoint.env;
     } 
 
+    // TODO move adapater to separate file
     let stream = flow(entrypoint.event, {
         mic: (name, callback) => {
             callback(null, require(entrypoint.network + '/' + name));
@@ -21,4 +49,4 @@ process.on('message', (entrypoint) => {
     });
     stream.on('error', process.stderr.write.bind(process.stderr));
     stream.end(1);
-});
+}
