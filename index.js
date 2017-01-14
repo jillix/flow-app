dc6420a51a5beb53cbe3477a4daa7c0278586584
Adrian Ottiker <adrian@ottiker.com>
2017-01-14 21:32:19 +0100

    create store instance with config file data

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
 
 const resolve = require('path').resolve;
 const Flow = require(__dirname + '/node_modules/flow');
-const FlowApi = require(__dirname + '/node_modules/flow-api');
+const Store = require(__dirname + '/node_modules/flow-api/lib/store');
 const Adapter = require(__dirname + '/lib/adapter');
 const Entrypoint = require(__dirname + '/lib/entrypoint');
 const entrypoint_name = process.argv[2];
 
-// TODO choose store?
-// config file or environment
-const store = FlowApi.Store({
-    store: "cayley",
-    config: "http://localhost:64210"
-});
+if (!entrypoint_name) {
+    process.stderr.write('Missing entrypoint argument.');
+    process.exit(0);
+}
 
-!entrypoint_name && error('Missing entrypoint argument.');
+// create store instance from config file
+const store = Store(require(__dirname + '/config.json'));
+
+// get entrypoint
 Entrypoint(store, entrypoint_name, (entrypoint) => {
 
     entrypoint.base = __dirname;
 
+    // init flow and emit first sequence
     let flow = Flow(entrypoint.env, Adapter(store, entrypoint))(entrypoint.sequence);
     flow.on('data', chunk => process.stdout.write(chunk.toString()));
     flow.on('error', error => process.stderr.write(error.stack.toString() + '\n'));
