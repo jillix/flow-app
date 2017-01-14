aeb5735e103a21aec028a40ab9e485f6a0e37f5e
Adrian Ottiker <adrian@ottiker.com>
2017-01-14 21:07:44 +0100

    create new store, to get entrypoint and sequences

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 
 const resolve = require('path').resolve;
 const Flow = require(__dirname + '/node_modules/flow');
+const FlowApi = require(__dirname + '/node_modules/flow-api');
 const Adapter = require(__dirname + '/lib/adapter');
 const Entrypoint = require(__dirname + '/lib/entrypoint');
 const entrypoint_name = process.argv[2];
-const base_path = resolve(process.argv[3] || '.');
+
+// TODO choose store?
+// config file or environment
+const store = FlowApi.Store({
+    store: "cayley",
+    config: "http://localhost:64210"
+});
 
 !entrypoint_name && error('Missing entrypoint argument.');
-Entrypoint(entrypoint_name, entrypoint => {
+Entrypoint(store, entrypoint_name, (entrypoint) => {
 
-    entrypoint.base = base_path;
+    entrypoint.base = __dirname;
 
-    let flow = Flow(entrypoint.env, Adapter(entrypoint))(entrypoint.sequence);
+    let flow = Flow(entrypoint.env, Adapter(store, entrypoint))(entrypoint.sequence);
     flow.on('data', chunk => process.stdout.write(chunk.toString()));
     flow.on('error', error => process.stderr.write(error.stack.toString() + '\n'));
     flow.end(1);
