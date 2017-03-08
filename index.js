986c8230bdb2d7440158bc323380a5e2fd279bbb
Adrian Ottiker <adrian@ottiker.com>
2017-03-09 00:34:51 +0100

    sequences can now have arguments, this makes entrypoint redundant

--- index.js
+++ index.js
@@ -2,33 +2,27 @@
 
 const resolve = require('path').resolve;
 const Flow = require(__dirname + '/node_modules/flow');
-const Store = require(__dirname + '/node_modules/flow-api/lib/store');
 const Adapter = require(__dirname + '/lib/adapter');
-const Entrypoint = require(__dirname + '/lib/entrypoint');
-const entrypoint_name = process.argv[2];
+const sequence_id = process.argv[2];
 const application_dir = resolve(process.argv[3] || '.');
 
-if (!entrypoint_name) {
-    process.stderr.write('Missing entrypoint argument.');
+if (!sequence_id) {
+    process.stderr.write('Start sequence missing. Example: flow sequenceId');
     process.exit(0);
 }
 
-// create store instance from config file
-const store = Store(require(__dirname + '/config.json'));
+// TODO get sequence id by name, or just ID?
 
-// get entrypoint
-Entrypoint(store, entrypoint_name, (entrypoint) => {
-
-    entrypoint.base = __dirname;
-    entrypoint.module_root = entrypoint.base + '/node_modules/';
-    entrypoint.env._modDir = entrypoint.module_root;
-    entrypoint.env._appDir = application_dir;
-
-    // init flow and emit first sequence
-    const event = Flow(entrypoint.env, Adapter(store, entrypoint))(entrypoint.sequence)
-    process.stdin.pipe(event).pipe(process.stdout);
-    event.on("error", (err) => {
-        err = err.stack ? err.stack : err;
-        process.stderr.write(err.toString());
-    });
+//entrypoint.env._modDir = entrypoint.module_root;
+//entrypoint.env._appDir = application_dir;
+ 
+// init flow and emit first sequence
+const event = Flow(Adapter(application_dir))({
+    sequence: sequence_id,
+    role: '_:3389dae361af79b04c9c8e7057f60cc6'
+});
+process.stdin.pipe(event).pipe(process.stdout);
+event.on("error", (err) => {
+    err = err.stack ? err.stack : err;
+    process.stderr.write(err.toString());
 });
