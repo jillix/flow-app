8978e5f5f32c2b83b84a40ea1ddc79f8d9d959ef
Adrian Ottiker <adrian@ottiker.com>
2017-06-10 15:13:58 +0200

    remove nodejs cluster

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 #!/usr/bin/env node
 
-/*const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const module_root = __dirname + "/node_modules/";
+const Flow = require(module_root + "flow");
+const promisify = require("util").promisify;
+const readFile = promisify(require("fs").readFile);
+const resolve = require("path").resolve;
+const handler = require("./lib/handler");
+const LRU = require("lru-cache");
+const sequence_id = process.argv[2];
+const base_path = resolve(process.argv[3] || ".");
 
-if (cluster.isMaster) {
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-} else {*/
+if (!sequence_id) {
+    process.stderr.write("Start sequence missing. Example: flow sequenceId");
+    process.exit(0);
+}
 
-    const module_root = __dirname + "/node_modules/"
-    const promisify = require('util').promisify;
-    const readFile = promisify(require("fs").readFile);
-    const resolve = require("path").resolve;
-    const Flow = require(module_root + "flow");
-    const Registry = require(module_root + "flow-registry");
-    const LRU = require("lru-cache");
-    const sequence_id = process.argv[2];
-    const base_path = resolve(process.argv[3] || '.');
+// set base path in evnironment
+process.env.flow_base = base_path;
+const event = Flow({
+    cache: LRU({max: 500}),
+    seq: (sequence_id, role) => {
+        return readFile(base_path + "/" + sequence_id + ".json").then(JSON.parse);
+    },
+    fn: handler
+})({
+    sequence: sequence_id,
+    role: "_:3389dae361af79b04c9c8e7057f60cc6",
+    base: base_path
+})
 
-    if (!sequence_id) {
-        process.stderr.write('Start sequence missing. Example: flow sequenceId');
-        process.exit(0);
-    }
+/*event.catch((err) => {
+    err = err.stack ? err.stack : err;
+    process.stderr.write(err.toString());
+});*/
 
-    // set base path in evnironment
-    process.env.flow_base = base_path;
-    const event = Flow({
-        cache: LRU({max: 500}),
-        seq: (sequence_id, role) => {
-            return readFile(base_path + "/" + sequence_id + ".json").then(JSON.parse);
-        },
+event.on("error", (err) => {
+    err = err.stack ? err.stack : err;
+    process.stderr.write(err.toString());
+});
 
-        fn: Registry.getFn
-    })({
-        sequence: sequence_id,
-        role: '_:3389dae361af79b04c9c8e7057f60cc6',
-        base: base_path
-    });
-
-    event.on("error", (err) => {
-        err = err.stack ? err.stack : err;
-        process.stderr.write(err.toString());
-    });
-
-    process.stdin.pipe(event).pipe(process.stdout);
-//}
+process.stdin.pipe(event).pipe(process.stdout);
