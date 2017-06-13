7d9b078c8d7b047c6e23dfc0e25622b63cf9a758
Adrian Ottiker <adrian@ottiker.com>
2017-06-13 16:22:36 +0200

    move cache methods to adapter root

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+"use strict"
 
-const module_root = __dirname + "/node_modules/";
-const Flow = require(module_root + "flow");
+require(__dirname + "/node_modules/" + "flow");
+
 const promisify = require("util").promisify;
 const readFile = promisify(require("fs").readFile);
 const resolve = require("path").resolve;
@@ -9,6 +10,7 @@ const handler = require("./lib/handler");
 const LRU = require("lru-cache");
 const sequence_id = process.argv[2];
 const base_path = resolve(process.argv[3] || ".");
+const lru = LRU({max: 500});
 
 if (!sequence_id) {
     process.stderr.write("Start sequence missing. Example: flow sequenceId");
@@ -17,26 +19,19 @@ if (!sequence_id) {
 
 // set base path in evnironment
 process.env.flow_base = base_path;
-const event = Flow({
-    cache: LRU({max: 500}),
+Flow({
+    set: lru.set.bind(lru),
+    get: lru.get.bind(lru),
+    del: lru.del.bind(lru),
     seq: (sequence_id, role) => {
         return readFile(base_path + "/" + sequence_id + ".json").then(JSON.parse);
     },
-    fn: handler
+    fnc: handler
 })({
     sequence: sequence_id,
-    role: "_:3389dae361af79b04c9c8e7057f60cc6",
+    role: "3389dae361af79b04c9c8e7057f60cc6",
     base: base_path
-})
-
-/*event.catch((err) => {
+}).catch((err) => {
     err = err.stack ? err.stack : err;
-    process.stderr.write(err.toString());
-});*/
-
-event.on("error", (err) => {
-    err = err.stack ? err.stack : err;
-    process.stderr.write(err.toString());
+    process.stderr.write(err.toString() + "\n");
 });
-
-process.stdin.pipe(event).pipe(process.stdout);
