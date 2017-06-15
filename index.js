f3779af5ffcd30067d2575b0f93e6729fe91acf0
Adrian Ottiker <adrian@ottiker.com>
2017-06-15 20:39:27 +0200

    testing adapter.fnc method

--- index.js
+++ index.js
@@ -6,32 +6,56 @@ require(__dirname + "/node_modules/" + "flow");
 const promisify = require("util").promisify;
 const readFile = promisify(require("fs").readFile);
 const resolve = require("path").resolve;
-const handler = require("./lib/handler");
-const LRU = require("lru-cache");
+const dependency = require("./lib/dependency");
+//const LRU = require("lru-cache");
 const sequence_id = process.argv[2];
 const base_path = resolve(process.argv[3] || ".");
-const lru = LRU({max: 500});
+const handler_base = resolve((process.argv[3] || ".") + "../handlers");
+//const lru = LRU({max: 500});
+const cache = {};
 
 if (!sequence_id) {
     process.stderr.write("Start sequence missing. Example: flow sequenceId");
     process.exit(0);
 }
 
+console.log("HANDLER BASE:", handler_base);
+
 // set base path in evnironment
 process.env.flow_base = base_path;
 Flow({
-    set: lru.set.bind(lru),
-    get: lru.get.bind(lru),
-    del: lru.del.bind(lru),
+    set: (key, val) => {
+        cache[key] = val;
+    },
+    get: (key) => {
+        return cache[key];
+    },
+    del: (key) => {
+        delete cache[key];
+    },
     seq: (sequence_id, role) => {
         return readFile(base_path + "/" + sequence_id + ".json").then(JSON.parse);
     },
-    fnc: handler
+    fnc: (fn_iri, role) => {
+        // TODO local or over a network?
+        return new Promise((resolve, reject) => {
+            process.nextTick(()=>{
+                require(handler_base + "/" + fn_iri);
+                resolve(fn_iri);
+            });
+        });
+    },
+    // TODO install deps locally
+    dep: dependency
 })({
     sequence: sequence_id,
     role: "3389dae361af79b04c9c8e7057f60cc6",
     base: base_path
-}).catch((err) => {
+})
+.then((output) => {
+    console.log("Flow output:", output);
+})
+.catch((err) => {
     err = err.stack ? err.stack : err;
     process.stderr.write(err.toString() + "\n");
 });
